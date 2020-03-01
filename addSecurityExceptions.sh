633930ac7eecc5930f1f493c2179aa36bd1ded93
Itay Levy <itayl@ironsrc.com>
2020-02-29 17:58:36 -0800

    merge from stash

--- addSecurityExceptions.sh
+++ addSecurityExceptions.sh
@@ -30,13 +30,19 @@ new="_new.apk"
 newFileName=$filename$new
 tmpDir=/tmp/$filename
 
-java -jar "$DIR/apktool.jar" d -f -s -o "$tmpDir" "$fullfile"
+java -jar "$DIR/apktool.jar" d -s -f -o "$tmpDir" "$fullfile"
 
 if [ ! -d "$tmpDir/res/xml" ]; then
 	mkdir "$tmpDir/res/xml"
 fi
 
-cp "$DIR/network_security_config.xml" "$tmpDir/res/xml/."
+if [ ! -f "$tmpDir/res/xml/network_security_config.xml" ]; then 
+  cp "$DIR/network_security_config.xml" "$tmpDir/res/xml/."
+else
+  sed -E "s/(<network-security-config>)(.*)/\1  <base-config> <trust-anchors><certificates src=\"system\" \/><certificates src=\"user\" \/><\/trust-anchors><\/base-config><debug-overrides><trust-anchors> <certificates src=\"user\" \/> <\/trust-anchors> <\/debug-overrides>\2 /" "$tmpDir/res/xml/network_security_config.xml" > "$tmpDir/res/xml/network_security_config.xml.new" 
+  mv "$tmpDir/res/xml/network_security_config.xml.new" "$tmpDir/res/xml/network_security_config.xml" 
+fi
+
 if ! grep -q "networkSecurityConfig" "$tmpDir/AndroidManifest.xml"; then
   sed -E "s/(<application.*)(>)/\1 android\:networkSecurityConfig=\"@xml\/network_security_config\" \2 /" "$tmpDir/AndroidManifest.xml" > "$tmpDir/AndroidManifest.xml.new"
   mv "$tmpDir/AndroidManifest.xml.new" "$tmpDir/AndroidManifest.xml"
