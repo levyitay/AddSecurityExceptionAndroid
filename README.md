8fad2a3d662ecbfcf2504c8569938a866a04133d
Pablo Osés <opablo@gmail.com>
2020-08-29 21:39:23 -0300

    New -d param to make new apk debuggeable. Signing less verbose. More progress logging.

--- README.md
+++ README.md
@@ -1,31 +1,32 @@
 # Add Security Exception to APK
 
 In Android 7.0, Google introduced changes to the way user Certificate Authorities (CA) are trusted. These changes prevent third-parties from listening to network requests coming out of the application:
-More info: 
-1) https://developer.android.com/training/articles/security-config.html
-2) http://android-developers.blogspot.com/2016/07/changes-to-trusted-certificate.html
+More info:
+
+1. https://developer.android.com/training/articles/security-config.html
+2. http://android-developers.blogspot.com/2016/07/changes-to-trusted-certificate.html
 
 This script injects into the APK network security exceptions that allow third-party software like Charles Proxy/Fiddler to listen to the network requests and responses of some Android applications.
-
 
 ## Getting Started
 
 Download the script and the XML file and place them in the same directory.
 
 ### Prerequisites
+
 APKTOOL is not needed anymore.
 
 ~~You will need `apktool` and the Android SDK installed~~
-
 ~~I recommend using `brew` on Mac to install `apktool`:~~
-
-~~```brew install apktool```~~
+~~`brew install apktool`~~
 
 ## Usage
 
-The script take two arguments: 
-1) APK file path.
-2) keystore file path (**optional** - Default is: ~/.android/debug.keystore )
+The script takes three arguments:
+
+1. (optional) -d to also make the new APK debuggeable
+2. (required) APK file path.
+3. (optional) keystore file path (**optional** - Default is: ~/.android/debug.keystore )
 
 ### Examples
 
@@ -34,6 +35,6 @@ The script take two arguments:
 
 or
 
-./addSecurityExceptions.sh myApp.apk ~/.android/debug.keystore
+./addSecurityExceptions.sh -d myApp.apk ~/.android/debug.keystore
 
 ```
--- addSecurityExceptions.sh
+++ addSecurityExceptions.sh
@@ -3,12 +3,24 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ $# -eq 0 ]
   then
-    echo "No arguments supplied"
-    echo "Usage: $0 <APK filename>"
+    echo ""
+    echo "Usage: $0 [-d] <APK filename> [Signing Keystore]"
+    echo ""
+    echo "Options:"
+    echo "  -d      Make the new APK also debuggeable"
+    echo ""
     exit -1
 fi
+
+if [[ "$1" == "-d" ]]; then 
+	echo "Scheduled to make the new APK also debuggeable."
+	makeDebuggeable=true
+	shift
+fi
+
 if [ ! -z "$2" ]
 	then
+		echo "Scheduled to use custom keystore for signing."
 		debugKeystore=$2
 	else
     if [ ! -f ~/.android/debug.keystore ]; then
@@ -18,6 +30,7 @@ if [ ! -z "$2" ]
       echo "No debug keystore was found, creating new one..."
       keytool -genkey -v -keystore ~/.android/debug.keystore -storepass android -alias androiddebugkey -keypass android -keyalg RSA -keysize 2048 -validity 10000
     fi
+		echo "Scheduled to use debug keystore for signing."
 		debugKeystore=~/.android/debug.keystore
 fi
 
@@ -38,13 +51,19 @@ fi
 
 cp "$DIR/network_security_config.xml" "$tmpDir/res/xml/."
 if ! grep -q "networkSecurityConfig" "$tmpDir/AndroidManifest.xml"; then
+  echo "Injecting the networkSecurityConfig attribute in AndroidManifest.xml..."
   sed -E "s/(<application.*)(>)/\1 android\:networkSecurityConfig=\"@xml\/network_security_config\" \2 /" "$tmpDir/AndroidManifest.xml" > "$tmpDir/AndroidManifest.xml.new"
   mv "$tmpDir/AndroidManifest.xml.new" "$tmpDir/AndroidManifest.xml"
 fi
 
+if [ $makeDebuggeable ] && ! grep -q "debuggable" "$tmpDir/AndroidManifest.xml"; then
+  echo "Injecting the debuggable attribute in AndroidManifest.xml..."
+  sed -E "s/(<application.*)(>)/\1 android\:debuggable=\"true\" \2 /" "$tmpDir/AndroidManifest.xml" > "$tmpDir/AndroidManifest.xml.new"
+  mv "$tmpDir/AndroidManifest.xml.new" "$tmpDir/AndroidManifest.xml"
+fi
 
 java -jar "$DIR/apktool.jar" empty-framework-dir --force "$tmpDir"
 echo "Building new APK $newFileName"
 java -jar "$DIR/apktool.jar" b -o "./$newFileName" "$tmpDir"
-jarsigner -verbose -keystore $debugKeystore -storepass android -keypass android "./$newFileName" androiddebugkey
+jarsigner -keystore $debugKeystore -storepass android -keypass android "./$newFileName" androiddebugkey
 
